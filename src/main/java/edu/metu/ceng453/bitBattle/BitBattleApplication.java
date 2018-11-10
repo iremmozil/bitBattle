6f4bf79d7146fa1e588fadf5f0a6e5b3115f5747
e209967 <e209967@metu.edu.tr>
2018-11-10 17:02:28 +0300

    Add Spring Initialzr

---
+++ src/main/java/edu/metu/ceng453/bitBattle/BitBattleApplication.java
@@ -0,0 +1,12 @@
+package edu.metu.ceng453.bitBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitBattleApplication.class, args);
+	}
+}
