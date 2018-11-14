b5a26101f0d5e6871424a97320d646ed76cbd8d3
sonatbaltaci <sonat.baltaci@gmail.com>
2018-11-15 00:06:09 +0300

    Add gameID and set/get methods

--- src/main/java/edu/metu/ceng453/bitBattle/Leaderboard.java
+++ src/main/java/edu/metu/ceng453/bitBattle/Leaderboard.java
@@ -7,6 +7,7 @@ import java.util.Date;
 public class Leaderboard {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer gameID;
 
     @OneToMany
     @JoinColumn (name = "playerID")
@@ -16,22 +17,42 @@ public class Leaderboard {
     private Date gameTime;
 
 
-
-    Leaderboard(Integer playerID, Integer score, Date gameTime) {
+    Leaderboard(Integer gameID, Integer playerID, Integer score, Date gameTime) {
+        this.gameID = gameID;
         this.playerID = playerID;
         this.score = score;
         this.gameTime = gameTime;
+    }
+
+    public Integer getID() {
+        return gameID;
+    }
+
+    public void setID(Integer gameID) {
+        this.gameID = gameID;
     }
 
     public Integer getPlayerID() {
         return playerID;
     }
 
-    public Date getGameTime() {
-        return gameTime;
+    public void setPlayerID(Integer playerID) {
+        this.playerID = playerID;
     }
 
     public Integer getScore() {
         return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Date getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime() {
+        this.gameTime = gameTime;
     }
 }
