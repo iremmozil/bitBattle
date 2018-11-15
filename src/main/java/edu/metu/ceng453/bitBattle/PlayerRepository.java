0233b73dd09a36280b858d3b1e82882b58ac0450
sonatbaltaci <sonat.baltaci@gmail.com>
2018-11-15 17:33:34 +0300

    Fix findbyID error

--- src/main/java/edu/metu/ceng453/bitBattle/PlayerRepository.java
+++ src/main/java/edu/metu/ceng453/bitBattle/PlayerRepository.java
@@ -3,5 +3,5 @@ package edu.metu.ceng453.bitBattle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface PlayerRepository extends JpaRepository<Player, Integer> {
-    Player findByID(Integer playerID);
+
 }
