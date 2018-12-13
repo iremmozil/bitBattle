43c12945cfdaa9786378b26d163c09e13c5de6df
iremmozil <iremmozil@gmail.com>
2018-12-13 20:35:20 +0300

    Change scene implementation

--- frontend/src/edu/metu/ceng453/bitBattle/Controller.java
+++ frontend/src/edu/metu/ceng453/bitBattle/Controller.java
@@ -1,4 +1,30 @@
 package edu.metu.ceng453.bitBattle;
 
-public class Controller {
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Controller extends Main{
+    @FXML
+    Button sbutton;
+
+
+    public void sbuttonPushed(ActionEvent event) throws IOException{
+        Parent levelOne = FXMLLoader.load(getClass().getResource("./levelOne.fxml"));
+        Scene sceneOne = new Scene(levelOne);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(sceneOne);
+        window.show();
+    }
 }
