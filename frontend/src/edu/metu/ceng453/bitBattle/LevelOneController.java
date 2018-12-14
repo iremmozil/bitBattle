208f1db3cb4e437ca1b255e963f49d5124100327
iremmozil <iremmozil@gmail.com>
2018-12-15 01:36:42 +0300

    A try to move aliens together

--- frontend/src/edu/metu/ceng453/bitBattle/LevelOneController.java
+++ frontend/src/edu/metu/ceng453/bitBattle/LevelOneController.java
@@ -3,9 +3,11 @@ package edu.metu.ceng453.bitBattle;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.CacheHint;
-import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -13,8 +15,11 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 
 
 public class LevelOneController extends Controller{
@@ -24,6 +29,9 @@ public class LevelOneController extends Controller{
 
     @FXML
     AnchorPane anchorOne;
+
+    @FXML
+    Button newButton;
 
     @FXML ImageView ailen1;
     @FXML ImageView ailen2;
@@ -37,76 +45,48 @@ public class LevelOneController extends Controller{
     @FXML ImageView ailen10;
     @FXML ImageView ailen11;
 
-    Double alienMove = 48.0;
+    Group alienGroup = new Group();
 
+
+
+    public void newButtonPushed(ActionEvent event) throws IOException {
+        Parent levelOne = FXMLLoader.load(getClass().getResource("./levelOne.fxml"));
+        Scene sceneOne = new Scene(levelOne);
+        sceneOne.getRoot().requestFocus();
+        Node node = ((Node)event.getSource());
+        Stage window = (Stage) (node.getScene().getWindow());
+        window.setScene(sceneOne);
+        window.show();
+    }
 
 
      public void animateLogo() {
+         alienGroup.getChildren().add(ailen1);
+         alienGroup.getChildren().add(ailen2);
+         alienGroup.getChildren().add(ailen3);
+         alienGroup.getChildren().add(ailen4);
+         alienGroup.getChildren().add(ailen5);
+         alienGroup.getChildren().add(ailen6);
+         alienGroup.getChildren().add(ailen7);
+         alienGroup.getChildren().add(ailen8);
+         alienGroup.getChildren().add(ailen9);
+         alienGroup.getChildren().add(ailen10);
+         alienGroup.getChildren().add(ailen11);
 
          PathTransition tt =
-                new PathTransition(Duration.seconds(5), new Line(),ailen1);
-         PathTransition tt2 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen2);
-         PathTransition tt3 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen3);
-         PathTransition tt4 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen4);
-         PathTransition tt5 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen5);
-         PathTransition tt6 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen6);
-         PathTransition tt7 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen7);
-         PathTransition tt8 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen8);
-         PathTransition tt9 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen9);
-         PathTransition tt10 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen10);
-         PathTransition tt11 =
-                 new PathTransition(Duration.seconds(5), new Line(),ailen11);
+                new PathTransition(Duration.seconds(5), new Line(),alienGroup);
 
-         tt.setPath(new Circle(20));
-         tt2.setPath(new Circle(20));
-         tt3.setPath(new Circle(20));
-         tt4.setPath(new Circle(20));
-         tt5.setPath(new Circle(20));
-         tt6.setPath(new Circle(20));
-         tt7.setPath(new Circle(20));
-         tt8.setPath(new Circle(20));
-         tt9.setPath(new Circle(20));
-         tt10.setPath(new Circle(20));
-         tt11.setPath(new Circle(20));
+         tt.setPath(new Rectangle(20,15));
 
         tt.setCycleCount( Timeline.INDEFINITE );
-         tt2.setCycleCount( Timeline.INDEFINITE );
-         tt3.setCycleCount( Timeline.INDEFINITE );
-         tt4.setCycleCount( Timeline.INDEFINITE );
-         tt5.setCycleCount( Timeline.INDEFINITE );
-         tt6.setCycleCount( Timeline.INDEFINITE );
-         tt7.setCycleCount( Timeline.INDEFINITE );
-         tt8.setCycleCount( Timeline.INDEFINITE );
-         tt9.setCycleCount( Timeline.INDEFINITE );
-         tt10.setCycleCount( Timeline.INDEFINITE );
-         tt11.setCycleCount( Timeline.INDEFINITE );
-
         tt.play();
-         tt2.play();
-         tt3.play();
-         tt4.play();
-         tt5.play();
-         tt6.play();
-         tt7.play();
-         tt8.play();
-         tt9.play();
-         tt10.play();
-         tt11.play();
 
     }
 
     @FXML
     private void setKeyListener(KeyEvent event){
         animateLogo();
+
         if (event.getCode() == KeyCode.RIGHT){
             double x = spaceship.getLayoutX();
             double y = spaceship.getLayoutY();
