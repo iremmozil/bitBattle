d4a55d1cb47e79ed95b40c099cecb4a0a7650a49
iremmozil <iremmozil@gmail.com>
2018-12-16 23:43:01 +0300

    Add player class

--- frontend/src/edu/metu/ceng453/bitBattle/Main.java
+++ frontend/src/edu/metu/ceng453/bitBattle/Main.java
@@ -6,9 +6,68 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+class Player {
+    public Player() {
+    }
+
+    private Integer id;
+
+    private String playername;
+
+    private String password;
+
+    private Integer highScore;
+
+    Player(String playerName, String playerPassword, Integer highScore) {
+        this.playername = playerName;
+        this.password = playerPassword;
+        this.highScore = highScore;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer playerId) {
+        this.id = playerId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String playerPassword) {
+        this.password = playerPassword;
+    }
+
+    public String getPlayername() {
+        return playername;
+    }
+
+    public void setPlayername(String playername) {
+        this.playername = playername;
+    }
+
+    public Integer getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(Integer highScore) {
+        this.highScore = highScore;
+    }
+}
 
 
 public class Main extends Application {
+    private static Player player;
+
+    public static Player getCurrentPlayer() {
+        return Main.player;
+    }
+
+    public static void setCurrentPlayer(Player newPlayer) {
+        Main.player = newPlayer;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
