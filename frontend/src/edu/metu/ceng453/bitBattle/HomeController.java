c77923363b0df129f4a9e609c61ac86ee19fa6f4
sonatbaltaci <sonat.baltaci@gmail.com>
2018-12-16 23:10:57 +0300

    Add comments and button handlers for Leaderboard and Log Out

--- frontend/src/edu/metu/ceng453/bitBattle/HomeController.java
+++ frontend/src/edu/metu/ceng453/bitBattle/HomeController.java
@@ -1,5 +1,6 @@
 package edu.metu.ceng453.bitBattle;
 
+// Import necessary libraries
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,15 +14,38 @@ import javax.swing.plaf.basic.BasicButtonUI;
 import java.io.IOException;
 
 public class HomeController {
+
+    // Bind variables to related FXML entities
     @FXML
     Button newButton;
 
+    // "New Game" button push handler
     public void newButtonPushed(ActionEvent event) throws IOException {
         Parent levelOne = FXMLLoader.load(getClass().getResource("./levelOne.fxml"));
         Scene sceneOne = new Scene(levelOne);
         sceneOne.getRoot().requestFocus();
         Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
         window.setScene(sceneOne);
+        window.show();
+    }
+
+    // "Leaderboard" button push handler
+    public void lbuttonPushed(ActionEvent event) throws IOException {
+        Parent lBoard = FXMLLoader.load(getClass().getResource("./leaderboard.fxml"));
+        Scene sceneLBoard = new Scene(lBoard);
+        sceneLBoard.getRoot().requestFocus();
+        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
+        window.setScene(sceneLBoard);
+        window.show();
+    }
+
+    // "Log Out" button push handler
+    public void logoutbuttonPushed(ActionEvent event) throws IOException {
+        Parent signIn = FXMLLoader.load(getClass().getResource("./signin.fxml"));
+        Scene sceneSignIn = new Scene(signIn);
+        sceneSignIn.getRoot().requestFocus();
+        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
+        window.setScene(sceneSignIn);
         window.show();
     }
 }
