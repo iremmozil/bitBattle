bf4010bc3405ef3cf4c1b4e413396a410a8aec4d
iremmozil <iremmozil@gmail.com>
2019-01-14 19:21:57 +0300

    Add new method

--- frontend/src/edu/metu/ceng453/bitBattle/Alien.java
+++ frontend/src/edu/metu/ceng453/bitBattle/Alien.java
@@ -1,9 +1,17 @@
 package edu.metu.ceng453.bitBattle;
 
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public interface Alien {
-    public int health = 0;
+
+    int health = 0;
+    public double alienBulletRadious = 5.05;
+
     public void fire();
-
-
+    public void move();
+    public void alienShotDown(AnchorPane anchor, Node bullet);
 }
 
