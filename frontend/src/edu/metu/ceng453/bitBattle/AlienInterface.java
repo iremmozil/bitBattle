298e57845c75f86d94bc069a667330eddd2d777a
iremmozil <iremmozil@gmail.com>
2019-01-14 23:17:34 +0300

    Add Alien Interface class and add method declerations

--- frontend/src/edu/metu/ceng453/bitBattle/AlienInterface.java
+++ frontend/src/edu/metu/ceng453/bitBattle/AlienInterface.java
@@ -1,4 +1,23 @@
 package edu.metu.ceng453.bitBattle;
 
+import javafx.animation.PathTransition;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
+
 public interface AlienInterface {
+
+
+    public void fire(AnchorPane anchor);
+
+    public void alienShotDown(AnchorPane anchor, Node bullet);
+
+    public int getHealth();
+
+    public void setHealth(int health);
+
+    public void move();
 }
