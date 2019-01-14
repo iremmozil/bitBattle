fa73f80e284c322ce8d23cbf78447382ace81612
iremmozil <iremmozil@gmail.com>
2019-01-14 23:17:48 +0300

    Add contructor

--- frontend/src/edu/metu/ceng453/bitBattle/ParallelogramAlien.java
+++ frontend/src/edu/metu/ceng453/bitBattle/ParallelogramAlien.java
@@ -1,6 +1,13 @@
 package edu.metu.ceng453.bitBattle;
 
+import javafx.scene.Node;
+
 public class ParallelogramAlien extends Alien {
+
+
+    public ParallelogramAlien(Node n) {
+        this.node = n;
+    }
 
     @Override
     public void move() {
