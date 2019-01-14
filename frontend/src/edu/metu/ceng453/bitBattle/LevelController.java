60e4d14526a8f71edecfdbf1d5ee9fd5fbcd8396
iremmozil <iremmozil@gmail.com>
2019-01-15 00:06:52 +0300

    Remove some magic numbers, remove removeAlienFromArray method, remove every attribute that belongs to the circle alien, Add animateAlien, alienShot and aliensToArray Methods, connect alienSHo method to Alien class

--- frontend/src/edu/metu/ceng453/bitBattle/LevelController.java
+++ frontend/src/edu/metu/ceng453/bitBattle/LevelController.java
@@ -9,6 +9,8 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+
 public class LevelController extends Main {
 
     //Variables
@@ -16,13 +18,17 @@ public class LevelController extends Main {
     private Boolean isFinished = false;
     private int score = Main.fromMaingetScore();
     private int health = 3;
-    private Boolean isShot = false;
-    private Integer circles[] = new Integer[4];
+    private int windowLeftEdge = -20;
+    private int windowRightEdge = 480;
+    private int spaceShipMoveSize = 6;
+    private double bulletRadious = 5.05;
 
-    private boolean circle1HitOnce = false;
-    private boolean circle2HitOnce = false;
-    private boolean circle3HitOnce = false;
-    private boolean circle4HitOnce = false;
+    private Boolean isShot = false;
+
+    ArrayList<Alien> aliens = new ArrayList<Alien>();
+    ArrayList<Node> nodeAliens = new ArrayList<Node>();
+
+
 
     //Getters and setters
     int getScore() {
@@ -41,77 +47,56 @@ public class LevelController extends Main {
         this.health = health;
     }
 
-    void initializeCircles(int i, int j, int k, int l){
-        circles[0] = i;
-        circles[1] = j;
-        circles[2] = k;
-        circles[3] = l;
+    //get aliens to an array and create aliens
+    void aliensToArray(AnchorPane anchor){
+        //when new level is started empty the array
+        AlienFactory alienFactory = new AlienFactory();
+
+        for (Node node: anchor.getChildren()){
+            String tag = node.getId();
+            if (tag.equals("triangle") || tag.equals("circle") || tag.equals("parallelogram")){
+                nodeAliens.add(node);
+                aliens.add(alienFactory.createAlien(tag,node));
+            }
+        }
     }
 
-    boolean isAlienShot(Node o, Node aliens[], AnchorPane anchor) {
+    void animateAliens(){
+        for (Alien a: aliens){
+            a.move();
+        }
+    }
+
+
+    void alienShot(AnchorPane anchor){
+        for(Node o: anchor.getChildren()){
+            if (o.getId() == "bullet"){
+                if (isAlienShot(o, aliens, anchor)) break;
+            }
+        }
+    }
+
+
+    // if spaceship could shoot one of the aliens if yes return true else return false
+    boolean isAlienShot(Node o, ArrayList<Alien> aliens, AnchorPane anchor) {
         isShot =  false;
-        for (int i = 0; i < aliens.length; i++) {
-            if (anchor.getChildren().contains(aliens[i])) {
-                if (o.getBoundsInParent().intersects(aliens[i].getBoundsInParent())) {
-                    if (aliens[i].getId().equals("edu.metu.ceng453.bitBattle.triangle") ){
-                        removeAlien(anchor, o, aliens[i]);
-                        //removeAlienFromArray(aliens, i);
-                        setScore(getScore() + 5);
-                    }
-                    else if (aliens[i].getId().equals("circle")){
-                        if (circles[0] == i){
-                            if (circle1HitOnce){
-                                removeAlien(anchor, o, aliens[i]);
-                                setScore(getScore() + 10);
-                            } else {
-                                circle1HitOnce = true;
-                            }
-                        }
-                        else if (circles[1] == i){
-                            if (circle2HitOnce){
-                                removeAlien(anchor, o, aliens[i]);
-                                setScore(getScore() + 10);
-                            } else {
-                                circle2HitOnce = true;
-                            }
-                        }
-                        else if (circles[2] == i){
-                            if (circle3HitOnce){
-                                removeAlien(anchor, o, aliens[i]);
-                                setScore(getScore() + 10);
-                            } else {
-                                circle3HitOnce = true;
-                            }
-                        }
-                        else if (circles[3] == i){
-                            if (circle4HitOnce){
-                                removeAlien(anchor, o, aliens[i]);
-                                setScore(getScore() + 10);
-                            } else {
-                                circle4HitOnce = true;
-                            }
-                        }
-                    }
-                    isShot = true;
-                }
+        for (Alien alien: aliens){
+            if (anchor.getChildren().contains(alien)){
+                isShot = alien.isShotDown(anchor,o);
             }
         }
         return isShot;
     }
 
+    //remove alien from anchorPane
     private void removeAlien(AnchorPane anchor, Node o, Node alien){
         anchor.getChildren().remove(o);
         anchor.getChildren().remove(alien);
     }
-    private void removeAlienFromArray(Node aliens[], int i ){
-        for (int j = i; j< aliens.length -2; j ++){
-            aliens[j] = aliens[j+1];
-        }
-        aliens[aliens.length-1] = null;
-    }
 
+    //When user presses SPACE spaceship should fire!
     void fire(AnchorPane anchor, ImageView spaceship){
-        Circle bullet = new Circle(5.05);
+        Circle bullet = new Circle(bulletRadious);
         bullet.setStroke(Color.BLACK);
         bullet.setStrokeWidth(0.0);
         bullet.setFill(Color.valueOf("997aff"));
@@ -126,18 +111,18 @@ public class LevelController extends Main {
         tt.play();
     }
 
+    //When user presses RIGHT or LEFT key calculate spaceship's new location
     double goDirection(double x, String direction){
         if( direction.equals("RIGHT")){
-            if (x < 480){
-                x = x + 6;
+            if (x < windowRightEdge){
+                x = x + spaceShipMoveSize;
             }
         }
         else if(direction.equals("LEFT")){
-            if( x > -20){
-                x = x - 6;
+            if( x > windowLeftEdge){
+                x = x - spaceShipMoveSize;
             }
         }
-
         return x;
     }
 
