bb29d99435715b6140d3ccbcee368f977bc50293
iremmozil <iremmozil@gmail.com>
2019-01-14 23:16:48 +0300

    Add create Alien method

--- frontend/src/edu/metu/ceng453/bitBattle/AlienFactory.java
+++ frontend/src/edu/metu/ceng453/bitBattle/AlienFactory.java
@@ -1,4 +1,23 @@
 package edu.metu.ceng453.bitBattle;
 
+import javafx.scene.Node;
+
 public class AlienFactory {
+
+    public Alien getAlien(String type, Node n){
+        if(type == null){
+            return null;
+        }
+        if(type.equalsIgnoreCase("circle")){
+            return new CircleAlien(n);
+
+        } else if(type.equalsIgnoreCase("triangle")){
+            return new TriangleAlien(n);
+
+        } else if(type.equalsIgnoreCase("parallelogram")){
+            return new ParallelogramAlien(n);
+        }
+
+        return null;
+    }
 }
