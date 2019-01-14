ea63cdd6cfd28c4c953df6d7edfcddc4339b5724
iremmozil <iremmozil@gmail.com>
2019-01-14 22:45:43 +0300

    Remove sme method implementations

--- frontend/src/edu/metu/ceng453/bitBattle/CircleAlien.java
+++ frontend/src/edu/metu/ceng453/bitBattle/CircleAlien.java
@@ -1,13 +1,24 @@
 package edu.metu.ceng453.bitBattle;
 
-public class CircleAlien implements Alien{
-    @Override
-    public void alienShooting() {
+import javafx.animation.PathTransition;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 
+public class CircleAlien extends Alien{
+
+
+    public CircleAlien(Node n) {
+        this.health = 2;
+        this.node = n;
     }
 
     @Override
     public void move() {
 
     }
+
 }
