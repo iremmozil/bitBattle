e287d14a3c676ea6daad64ab8a055901e1a857f8
iremmozil <iremmozil@gmail.com>
2019-01-15 00:44:57 +0300

    Move common methods to LevelController class, now they look simple

--- frontend/src/edu/metu/ceng453/bitBattle/LevelOneController.java
+++ frontend/src/edu/metu/ceng453/bitBattle/LevelOneController.java
@@ -60,25 +60,22 @@ public class LevelOneController extends LevelController {
     @FXML
     Button homeButton;
 
-    Node aliens[] = new Node[11];
-    Node aliensToShoot[] = new Node[11];
-
     private int Counter = 0;
     private Boolean isFinished = false;
-    private boolean dbUpdate = false;
+
     private boolean isAlienDown = false;
-    public int length = 11;
+
 
     public void initialize() {
-        aliensToArray(aliens, alien1, alien2, alien3, alien4, alien5, alien6, alien7, alien8, alien9, alien10, alien11);
-        aliensToShoot = aliens.clone();
+
+        aliensToArray(anchorOne);
 
         scoreLabel.setText(Integer.toString(getScore()));
         endLevel.setVisible(false);
         homeButton.setVisible(false);
         gameOver.setVisible(false);
+        animateAliens();
 
-        animatealiens();
         gridOne.setOnKeyPressed((KeyEvent event)->{
             double y = spaceship.getLayoutY();
             if (event.getCode() == KeyCode.RIGHT){
@@ -105,206 +102,28 @@ public class LevelOneController extends LevelController {
         new AnimationTimer() {
             @Override
             public void handle(long now) {
+
                 Counter++;
                 if (Counter % 1000 == 0){
                     alienRandomize(anchorOne);
                 }
-                isSpaceshipDown();
-                alienShot();
+                if (isSpaceshipDown(anchorOne, spaceship, healthCount)){
+                    gameOver.setVisible(true);
+                    homeButton.setVisible(true);
+                }
+
+                alienShot(anchorOne);
+
                 scoreLabel.setText(Integer.toString(getScore()));
-                isLevelFinished();
+                if (isLevelFinished(anchorOne)){
+                    endLevel.setVisible(true);
+                    setScore(getScore());
+                }
             }
         }.start();
     }
 
-    static void aliensToArray(Node[] aliens, ImageView alien1, ImageView alien2, ImageView alien3, ImageView alien4, ImageView alien5, ImageView alien6, ImageView alien7, ImageView alien8, ImageView alien9, ImageView alien10, ImageView alien11) {
-        aliens[0] = alien1;
-        aliens[1] = alien2;
-        aliens[2] = alien3;
-        aliens[3] = alien4;
-        aliens[4] = alien5;
-        aliens[5] = alien6;
-        aliens[6] = alien7;
-        aliens[7] = alien8;
-        aliens[8] = alien9;
-        aliens[9] = alien10;
-        aliens[10] = alien11;
-    }
 
-    private void alienShot(){
-        for(Node o: anchorOne.getChildren()){
-            if (o.getId() == "bullet"){
-                if (isAlienShot(o, aliens, anchorOne)) break;
-            }
-        }
-    }
-
-
-    private void isSpaceshipDown() {
-        boolean collisionDetected = false;
-        for (Node bullet : anchorOne.getChildren()) {
-            if(bullet != spaceship) {
-                if (spaceship.getBoundsInParent().intersects(bullet.getBoundsInParent())) {
-                    collisionDetected = true;
-                    anchorOne.getChildren().remove(bullet);
-                    break;
-                }
-            }
-        }
-
-        if (collisionDetected){
-            collisionDetected = false;
-            if (getHealth() > 0){
-                setHealth(getHealth() -1);
-                healthCount.setText(Integer.toString(getHealth()));
-            }
-            else{
-                gameOver.setVisible(true);
-                homeButton.setVisible(true);
-
-                CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-                try {
-                    if (!dbUpdate) {
-                        Main.getCurrentGame().setScore(getScore());
-                        if(Main.getCurrentPlayer().getHighScore() == null || Main.getCurrentPlayer().getHighScore()<getScore())
-                            Main.getCurrentPlayer().setHighScore(getScore());
-                        HttpPost gameRequest = new HttpPost("http://localhost:8080/leaderboard");
-
-                        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-                        String jsonInString = gson.toJson(Main.getCurrentGame());
-                        StringEntity params = new StringEntity(jsonInString);
-                        gameRequest.addHeader("content-type", "application/json");
-                        System.out.println(params);
-
-
-                        gameRequest.setEntity(params);
-                        httpClient.execute(gameRequest);
-                        System.out.println("POST Request Handling");
-
-                        HttpPut playerRequest = new HttpPut("http://localhost:8080/player/" + Main.getCurrentPlayer().getId());
-
-                        params = new StringEntity(Main.getCurrentPlayer().getHighScore().toString());
-                        playerRequest.addHeader("content-type", "application/json");
-                        playerRequest.setEntity(params);
-                        httpClient.execute(playerRequest);
-                        dbUpdate = true;
-                    }
-
-                } catch (Exception ex) {
-                    System.out.println(ex);
-                } finally {
-                    try {
-                        httpClient.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
-    private void isLevelFinished(){
-        ObservableList<Node> children = anchorOne.getChildren();
-        if (!children.contains(alien1) &&
-                !children.contains(alien2) &&
-                !children.contains(alien3) &&
-                !children.contains(alien4) &&
-                !children.contains(alien5) &&
-                !children.contains(alien6) &&
-                !children.contains(alien7) &&
-                !children.contains(alien8) &&
-                !children.contains(alien9) &&
-                !children.contains(alien10) &&
-                !children.contains(alien11)
-        ) {
-                endLevel.setVisible(true);
-                isFinished = true;
-                setScore(getScore());
-        }
-    }
-
-    private void alienRandomize(AnchorPane anchorOne){
-        Circle b = new Circle(5.05);
-        b.setStroke(Color.BLACK);
-        b.setStrokeWidth(0.0);
-        b.setFill(Color.valueOf("99daff"));
-        anchorOne.getChildren();
-        int n = (int)(Math.random() * 11 + 0);
-        double triangleHeight = 40.0;
-        double triangleHalfWidth = 22.0;
-        if (anchorOne.getChildren().contains(aliens[n])){
-            b.setCenterX(aliens[n].getLayoutX() + triangleHalfWidth + aliens[n].getTranslateX());
-            b.setCenterY(aliens[n].getLayoutY() + triangleHeight + aliens[n].getTranslateY());
-            anchorOne.getChildren().add(b);
-        }
-        double x = b.getCenterX();
-        double y = b.getCenterY();
-        PathTransition tt =
-                new PathTransition(Duration.seconds(3), new Line(x,y, x ,650),b);
-        tt.play();
-    }
-
-     private void animatealiens() {
-         PathTransition tt1 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien1);
-         PathTransition tt2 =
-                new PathTransition(Duration.seconds(5), new Line(),alien2);
-         PathTransition tt3 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien3);
-         PathTransition tt4 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien4);
-         PathTransition tt5 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien5);
-         PathTransition tt6 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien6);
-         PathTransition tt7 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien7);
-         PathTransition tt8 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien8);
-         PathTransition tt9 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien9);
-         PathTransition tt10 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien10);
-         PathTransition tt11 =
-                 new PathTransition(Duration.seconds(5), new Line(),alien11);
-
-         tt2.setPath(new Rectangle(20,15));
-         tt1.setPath(new Rectangle(20,15));
-         tt4.setPath(new Rectangle(20,15));
-         tt5.setPath(new Rectangle(20,15));
-         tt6.setPath(new Rectangle(20,15));
-         tt7.setPath(new Rectangle(20,15));
-         tt8.setPath(new Rectangle(20,15));
-         tt9.setPath(new Rectangle(20,15));
-         tt10.setPath(new Rectangle(20,15));
-         tt11.setPath(new Rectangle(20,15));
-         tt3.setPath(new Rectangle(20,15));
-
-
-        tt2.setCycleCount( Timeline.INDEFINITE );
-         tt1.setCycleCount( Timeline.INDEFINITE );
-         tt3.setCycleCount( Timeline.INDEFINITE );
-         tt4.setCycleCount( Timeline.INDEFINITE );
-         tt5.setCycleCount( Timeline.INDEFINITE );
-         tt6.setCycleCount( Timeline.INDEFINITE );
-         tt7.setCycleCount( Timeline.INDEFINITE );
-         tt8.setCycleCount( Timeline.INDEFINITE );
-         tt9.setCycleCount( Timeline.INDEFINITE );
-         tt10.setCycleCount( Timeline.INDEFINITE );
-         tt11.setCycleCount( Timeline.INDEFINITE );
-
-        tt2.play();
-         tt1.play();
-         tt3.play();
-         tt4.play();
-         tt5.play();
-         tt6.play();
-         tt7.play();
-         tt8.play();
-         tt9.play();
-         tt10.play();
-         tt11.play();
-    }
 
     public void secondLevel(KeyEvent event) throws IOException {
         Parent levelTwo = FXMLLoader.load(getClass().getResource("design/levelTwo.fxml"));
