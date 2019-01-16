1605eb157d97277d6e20d5907dc21b7b41b33dc2
sonatbaltaci <sonat.baltaci@gmail.com>
2019-01-16 21:25:23 +0300

    Build network communication between clients and Matchmaker

---
+++ multiplayer-server/src/MatchmakerServer.java
@@ -0,0 +1,130 @@
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MatchmakerServer {
+    static Server server;
+
+    private MatchmakerServer() {
+        server = new Server() {
+            protected Connection newConnection() {
+                return new PlayerConnection();
+            }
+        };
+
+        Event.register(server);
+
+        ArrayList<PlayerConnection> playerList = new ArrayList<>();
+        //Lock lock = new ReentrantLock(); // Create a lock
+        server.addListener(new Listener.ThreadedListener(new Listener() {
+            public void connected (Connection conn) {
+                // When a player connects, put player into list
+                PlayerConnection connection = (PlayerConnection) conn;
+                // TODO: Acquire lock here for playerList
+                //  lock.lock();
+                playerList.add(connection);
+                //  lock.unlock();
+            }
+
+            public void received (Connection conn, Object object) {
+                PlayerConnection connection = (PlayerConnection) conn;
+                if (object instanceof Event.MovePlayer) {
+                    server.sendToTCP(connection.opponent.getID(), object);
+                    return;
+                }
+
+                if (object instanceof Event.FirePlayer) {
+                    server.sendToTCP(connection.opponent.getID(), object);
+                    return;
+                }
+
+                if (object instanceof Event.AlienIsDead) {
+                    // TODO: Mark alien as dead
+                    connection.aliens.set(((Event.AlienIsDead) object).alienIndex,0);
+                }
+            }
+
+            public void disconnected (Connection conn) {
+                PlayerConnection connection = (PlayerConnection) conn;
+                // TODO: Acquire lock here for playerList
+                //  lock.lock();
+                playerList.remove(connection);
+                //  lock.unlock();
+            }
+        }));
+
+        new Thread( () -> {
+            PlayerConnection player1 = new PlayerConnection();
+            PlayerConnection player2 = new PlayerConnection();
+
+            // When playList has 2 entities, pop them and start the threads with player sockets
+            // TODO: Acquire lock here for playerList
+            //  lock.lock();
+            if (playerList.size() > 1) {
+                player1 = playerList.remove(0);
+                player2 = playerList.remove(0);
+            }
+            //  lock.unlock();
+
+            player1.opponent = player2;
+            player2.opponent = player1;
+            server.sendToTCP(player1.getID(), new Event.StartandMoveAliens());
+            server.sendToTCP(player2.getID(), new Event.StartandMoveAliens());
+
+            new Thread(new syncAlienTask(player1, player2)).start();
+        }).start();
+
+    }
+
+    static class PlayerConnection extends Connection {
+        PlayerConnection opponent;
+        int noOfAliens = 23;
+        ArrayList<Integer> aliens = new ArrayList<>(Collections.nCopies(noOfAliens, 1));
+    }
+
+    public static class syncAlienTask implements Runnable {
+        private PlayerConnection player1;
+        private PlayerConnection player2;
+
+        syncAlienTask(PlayerConnection player1, PlayerConnection player2) {
+            this.player1 = player1;
+            this.player2 = player2;
+
+        }
+
+        public void run() {
+            Event.AlienFired alienFired = new Event.AlienFired();
+
+            while (player1.isConnected() && player2.isConnected()) {
+                Random rand = new Random();
+                int ind = rand.nextInt(player1.aliens.size());
+                if (player1.aliens.get(ind) == 0 || player2.aliens.get(ind) == 0) {
+                    continue;
+                }
+                alienFired.alienIndex = ind;
+                server.sendToTCP(player1.getID(),alienFired);
+                server.sendToTCP(player2.getID(),alienFired);
+                // sleep a little bit
+            }
+
+            if (player1.isConnected()) {
+                server.sendToTCP(player2.getID(), new Event.OpponentDisconnected());
+                player1.close();
+            }
+
+            else if (player2.isConnected()) {
+                server.sendToTCP(player1.getID(), new Event.OpponentDisconnected());
+                player2.close();
+            }
+
+        }
+    }
+
+    public static void main (String[] args) {
+        new MatchmakerServer();
+    }
+}
